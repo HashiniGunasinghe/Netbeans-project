1e510cd68a135ff676cb978f86bbcbc6aebe7519
hashini gunasinghe <124341500+HashiniGunasinghe@users.noreply.github.com>
2024-04-30 21:51:02 +0530

    Add files via upload

---
+++ src/SQLCON.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kavi♥
+ */
+import java.sql.*;
+public class SQLCON {
+    public static Connection connect()
+    {
+   try
+{
+     Class.forName("com.mysql.jdbc.Driver");
+     Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/tea","root","");
+     return conn;
+}
+catch (Exception e)
+{
+return null;
+        }
+    }
+}
